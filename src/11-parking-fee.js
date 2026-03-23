2abd274392dd2975db5733a11402cf04540dc94e
SigmaZardan <bibekbhujel077@gmail.com>
2026-03-23 20:31:06 +0545

    Refractor code

--- src/11-parking-fee.js
+++ src/11-parking-fee.js
@@ -34,68 +34,23 @@
  */
 
 const VEHICLE_TYPE = {
-  CAR: "car",
-  MOTOR: "motorcycle",
-  BUS: "bus"
+    CAR:   "car",
+    MOTOR: "motorcycle",
+    BUS:   "bus"
 }
 
-  const FIRST_HOUR_CAR_FEE = 5
-  const FIRST_HOUR_MOTOR_FEE = 3
-  const FIRST_HOUR_BUS_FEE = 10
-  const CAR_DAILY_MAX_FEE = 30
-  const MOTOR_DAILY_MAX_FEE = 18
-  const BUS_DAILY_MAX_FEE = 60
-  const REMAINING_HOUR_CAR_FEE = 3
-  const REMAINING_HOUR_MOTOR_FEE = 2
-  const REMAINING_HOUR_BUS_FEE = 7
+const RATES = {
+    car:        { first: 5,  remaining: 3, max: 30 },
+    motorcycle: { first: 3,  remaining: 2, max: 18 },
+    bus:        { first: 10, remaining: 7, max: 60  }
+}
 
 export function calculateParkingFee(hours, vehicleType) {
-  hours = Math.ceil(hours)
-  if (hours <= 0) { return -1}
-  const isCar = vehicleType == VEHICLE_TYPE.CAR
-  const isMotorCycle = vehicleType == VEHICLE_TYPE.MOTOR
-  const isBus = vehicleType ==  VEHICLE_TYPE.BUS
-  if (!(isCar || isMotorCycle || isBus)) { return -1}
-  // Your code here
-  // if the fee is more than daily maximum then simply return daily maximum
-//    * Rates (first hour / each additional hour):
-//  *   - "car":        $5 first hour, then $3/hour
-//  *   - "motorcycle": $3 first hour, then $2/hour
-//  *   - "bus":        $10 first hour, then $7/hour
+    hours = Math.ceil(hours)
+    if (hours <= 0) return -1
+    if (!RATES[vehicleType]) return -1
 
-  return calculateFeesHelper(hours, vehicleType)
-}
-
-function calculateFeesHelper(hours, vehicleType) {
-  const remainingHour = hours - 1
-  let parkingFees = 0
-
-  if (vehicleType === VEHICLE_TYPE.CAR) {
-    parkingFees = FIRST_HOUR_CAR_FEE
-    const remainingHourCost = findRemainingHourCost(remainingHour, REMAINING_HOUR_CAR_FEE)
-    parkingFees += remainingHourCost
-    if (parkingFees > CAR_DAILY_MAX_FEE) { parkingFees = CAR_DAILY_MAX_FEE}
-  }
-  else if (vehicleType === VEHICLE_TYPE.MOTOR) {
-    parkingFees = FIRST_HOUR_MOTOR_FEE
-    const remainingHourCost = findRemainingHourCost(remainingHour, REMAINING_HOUR_MOTOR_FEE)
-    parkingFees += remainingHourCost
-    if (parkingFees > MOTOR_DAILY_MAX_FEE) { parkingFees = MOTOR_DAILY_MAX_FEE}
-  }
-  else {
-    parkingFees = FIRST_HOUR_BUS_FEE
-    const remainingHourCost = findRemainingHourCost(remainingHour, REMAINING_HOUR_BUS_FEE)
-    parkingFees += remainingHourCost
-    if (parkingFees > BUS_DAILY_MAX_FEE) { parkingFees = BUS_DAILY_MAX_FEE}
-  }
-
-  return parkingFees
-}
-
-function findRemainingHourCost(remainingHour, amountPerRemainingHour) {
-  let remainingHourCost = 0
-  for (let i = 1; i <= remainingHour; i++) {
-    remainingHourCost += amountPerRemainingHour
-  }
-  return remainingHourCost
+    const { first, remaining, max } = RATES[vehicleType]
+    const fee = first + (hours - 1) * remaining
+    return Math.min(fee, max)
 }
